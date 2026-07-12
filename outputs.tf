467453335daf78bf72523976748332e459268ad9
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:44 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "postgresql_virtual_network_rules" {
-  description = "All postgresql_virtual_network_rule resources"
-  value       = azurerm_postgresql_virtual_network_rule.postgresql_virtual_network_rules
-}
 output "postgresql_virtual_network_rules_ignore_missing_vnet_service_endpoint" {
-  description = "List of ignore_missing_vnet_service_endpoint values across all postgresql_virtual_network_rules"
-  value       = [for k, v in azurerm_postgresql_virtual_network_rule.postgresql_virtual_network_rules : v.ignore_missing_vnet_service_endpoint]
+  description = "Map of ignore_missing_vnet_service_endpoint values across all postgresql_virtual_network_rules, keyed the same as var.postgresql_virtual_network_rules"
+  value       = { for k, v in azurerm_postgresql_virtual_network_rule.postgresql_virtual_network_rules : k => v.ignore_missing_vnet_service_endpoint }
 }
 output "postgresql_virtual_network_rules_name" {
-  description = "List of name values across all postgresql_virtual_network_rules"
-  value       = [for k, v in azurerm_postgresql_virtual_network_rule.postgresql_virtual_network_rules : v.name]
+  description = "Map of name values across all postgresql_virtual_network_rules, keyed the same as var.postgresql_virtual_network_rules"
+  value       = { for k, v in azurerm_postgresql_virtual_network_rule.postgresql_virtual_network_rules : k => v.name }
 }
 output "postgresql_virtual_network_rules_resource_group_name" {
-  description = "List of resource_group_name values across all postgresql_virtual_network_rules"
-  value       = [for k, v in azurerm_postgresql_virtual_network_rule.postgresql_virtual_network_rules : v.resource_group_name]
+  description = "Map of resource_group_name values across all postgresql_virtual_network_rules, keyed the same as var.postgresql_virtual_network_rules"
+  value       = { for k, v in azurerm_postgresql_virtual_network_rule.postgresql_virtual_network_rules : k => v.resource_group_name }
 }
 output "postgresql_virtual_network_rules_server_name" {
-  description = "List of server_name values across all postgresql_virtual_network_rules"
-  value       = [for k, v in azurerm_postgresql_virtual_network_rule.postgresql_virtual_network_rules : v.server_name]
+  description = "Map of server_name values across all postgresql_virtual_network_rules, keyed the same as var.postgresql_virtual_network_rules"
+  value       = { for k, v in azurerm_postgresql_virtual_network_rule.postgresql_virtual_network_rules : k => v.server_name }
 }
 output "postgresql_virtual_network_rules_subnet_id" {
-  description = "List of subnet_id values across all postgresql_virtual_network_rules"
-  value       = [for k, v in azurerm_postgresql_virtual_network_rule.postgresql_virtual_network_rules : v.subnet_id]
+  description = "Map of subnet_id values across all postgresql_virtual_network_rules, keyed the same as var.postgresql_virtual_network_rules"
+  value       = { for k, v in azurerm_postgresql_virtual_network_rule.postgresql_virtual_network_rules : k => v.subnet_id }
 }
 
